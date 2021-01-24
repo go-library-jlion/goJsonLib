059833db05d455180e2bceb9dd56ce0b3331822d
xie hua liang <supper@9126.org>
2021-01-24 12:12:23 +0800

    aa

---
+++ Hash.go
@@ -0,0 +1,13 @@
+package goJsonLib
+import (
+"encoding/json"
+)
+
+//TrySerialization
+func TrySerialization(v interface{}) string {
+	result,error:=json.Marshal(v)
+	if(error==nil){
+		return ""
+	}
+	return string(result)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/go-library-jlion/goJsonLib
+
+go 1.15
